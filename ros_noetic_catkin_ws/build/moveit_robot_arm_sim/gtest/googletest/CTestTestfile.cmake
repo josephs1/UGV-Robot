06295cf1d92441ef106006d2c14ba488647c900c
josephs1 <joeystefanoni@gmail.com>
2025-02-19 14:51:38 -0500

    Added moveit package without EE.

---
+++ ros_noetic_catkin_ws/build/moveit_robot_arm_sim/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/jstefan1/Documents/CodeWorkspace/UGV-Robot/ros_noetic_catkin_ws/build/moveit_robot_arm_sim/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
