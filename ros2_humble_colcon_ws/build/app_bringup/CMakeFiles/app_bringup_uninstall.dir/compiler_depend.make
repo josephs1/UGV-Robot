76b59b19ee2571421cfb5a3cf8e7e7d768dc6b8b
josephs1 <joeystefanoni@gmail.com>
2025-03-24 21:47:23 -0400

    Ros2 publisher & subscriber nodes in ros package.

---
+++ ros2_humble_colcon_ws/build/app_bringup/CMakeFiles/app_bringup_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for app_bringup_uninstall.
+# This may be replaced when dependencies are built.
---
+++ ros2_humble_colcon_ws/build/app_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ugv-c7/CodeWorkspace/UGV-Robot/ros2_humble_colcon_ws/install/py_pub_sub/share/py_pub_sub/package.sh"
